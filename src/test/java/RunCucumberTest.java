aec207b80544a291b389542f5ff992bf3a319901
Joseph Weber <joseph.weber@townsq.com.br>
2022-09-13 15:32:47 -0300

    Init sonar config

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -5,6 +5,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "src/test/resources")
+        features = "src/test/resources/experiments")
 class RunCucumberTest {
 }
